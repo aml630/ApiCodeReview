47ad4ba2094600097aa178ad6a26f2bfbfbd909d
alex <alex.m.larson@gmail.com>
2016-03-18 10:14:19 -0700

    add profile showing functionality before showing repos

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,10 +1,20 @@
 var github = require('./../js/github.js').gitHubSearch;
+var githubUser = require('./../js/github.js').profileSearch;
 
 
 $(document).ready(function(){
+  $("#profileSearch").submit(function (event) {
+    event.preventDefault();
+    var username = $("#profileUserName").val();
+    githubUser(username);
+    $("#search").show();
+
+  });
+
   $("#search").submit(function (event) {
     event.preventDefault();
-    var username = $("#username").val();
+    var username = $("#profileUserName").val();
+    console.log(username);
     github(username);
   });
 });
--- js/github.js
+++ js/github.js
@@ -4,8 +4,6 @@ exports.gitHubSearch = function (userName) {
   $(".printPic").html(" ");
   $(".printName").html(" ")
   $(".printDescription").html(" ")
-
-
   $.get('https://api.github.com/users/'+userName+'/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
     $(".printPic").append("<img class = 'round' src = '"+ response[0].owner.avatar_url + "'>")
@@ -13,13 +11,28 @@ exports.gitHubSearch = function (userName) {
       $(".printName").append("<div class = 'project'><a href = '"+response[i].html_url+"'><li class = 'red'> Name: " + response[i].name + "</li></a>");
       $(".printDescription").append("<div class = 'project'><li> Description " + response[i].description + "</li></div>"  );
       $(".printDate").append("<div class = 'project'><li> Created At: " + moment(response[i].created_at).format("MMMM Do YYYY, h:mm:ss a") + "</li></div>"  );
-
-// 2016-01-13T00:03:52Z
     }
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+};
 
-  }).fail(function(error){
-    // console.log(error.responseJSON.message);
-  });
+exports.profileSearch = function (userName) {
+  $(".printProfile").text(" ");
+  $("#search").show();
+
+  $.get('https://api.github.com/users/'+userName+'?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    console.log(response.followers);
 
 
+    $(".printProfile").append("<img class = 'round' src = '"+ response.avatar_url + "'><li> Email: " + response.email + "</li><li> Name: " + response.name + "</li><li> Followers: " + response.followers + "</li><li> Following: " + response.following + "</li>")
+
+      // $(".printName").append("<div class = 'project'><a href = '"+response[i].html_url+"'><li class = 'red'> Name: " + response[i].name + "</li></a>");
+      // $(".printDescription").append("<div class = 'project'><li> Description " + response[i].description + "</li></div>"  );
+      // $(".printDate").append("<div class = 'project'><li> Created At: " + moment(response[i].created_at).format("MMMM Do YYYY, h:mm:ss a") + "</li></div>"  );
+
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
 };
