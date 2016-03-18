660ce020aaa4f202afd2ff50b9b402f63a1b5b04
alex <alex.m.larson@gmail.com>
2016-03-18 14:02:28 -0700

    simple pagination like effect working

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,5 +1,7 @@
 var github = require('./../js/github.js').gitHubSearch;
 var githubUser = require('./../js/github.js').profileSearch;
+var pagination = require('./../js/github.js').paginationSearch;
+
 
 
 $(document).ready(function() {
@@ -16,5 +18,15 @@ $(document).ready(function() {
         var username = $("#profileUserName").val();
         console.log(username);
         github(username);
+        $(".pagination").show();
+
+    });
+
+    $(".pagination li").click(function() {
+      var username = $("#profileUserName").val();
+      var thisVar = parseInt($(this).text());
+      console.log(thisVar)
+
+        pagination(username, thisVar);
     });
 });
--- js/github.js
+++ js/github.js
@@ -8,7 +8,27 @@ exports.gitHubSearch = function(userName) {
     $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response) {
         // console.log(response);
         $(".printPic").append("<img class = 'round' src = '" + response[0].owner.avatar_url + "'>");
-        for (var i = 0; i < response.length; i++) {
+        for (var i = 0; i < 5; i++) {
+            $(".printName").append("<div class = 'card-panel teal lighten-5 project'><a href = '" + response[i].html_url + "'><li> Name: " + response[i].name + "</li></a>");
+            $(".printDescription").append("<div class = 'card-panel  teal lighten-5 project'><li> Description " + response[i].description + "</li></div>");
+            $(".printDate").append("<div class = 'card-panel teal lighten-5 project'><li> Created At: " + moment(response[i].created_at).format("MMMM Do YYYY, h:mm:ss a") + "</li></div>");
+        }
+    }).fail(function(error) {
+        console.log(error.responseJSON.message);
+    });
+};
+exports.paginationSearch = function(userName, num) {
+    $(".printPic").html(" ");
+    $(".printName").html(" ");
+    $(".printDescription").html(" ");
+    $(".printDate").html(" ");
+    var thisVar = num;
+    console.log("log thisvar?: " + num);
+    var printout = 5*thisVar;
+    $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response) {
+        console.log(response);
+        $(".printPic").append("<img class = 'round' src = '" + response[0].owner.avatar_url + "'>");
+        for (var i = printout-5; i < printout; i++) {
             $(".printName").append("<div class = 'card-panel teal lighten-5 project'><a href = '" + response[i].html_url + "'><li> Name: " + response[i].name + "</li></a>");
             $(".printDescription").append("<div class = 'card-panel  teal lighten-5 project'><li> Description " + response[i].description + "</li></div>");
             $(".printDate").append("<div class = 'card-panel teal lighten-5 project'><li> Created At: " + moment(response[i].created_at).format("MMMM Do YYYY, h:mm:ss a") + "</li></div>");
