cba81f31b2496c85e207577cec5d967b3c65e144
alex <alex.m.larson@gmail.com>
2016-03-18 09:35:13 -0700

    add date show functionality

--- js/github.js
+++ js/github.js
@@ -1,12 +1,18 @@
 var apiKey  = require('./../.env').apiKey;
 
 exports.gitHubSearch = function (userName) {
+  $(".printPic").html(" ");
+  $(".printName").html(" ")
+  $(".printDescription").html(" ")
+
 
   $.get('https://api.github.com/users/'+userName+'/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
-    $(".printOut").append("<img class = 'round' src = '"+ response[0].owner.avatar_url + "'>")
+    $(".printPic").append("<img class = 'round' src = '"+ response[0].owner.avatar_url + "'>")
     for(var i = 0; i < response.length; i  ++) {
-      $(".printOut").append("<div class = 'project'><li class = 'red'> Name: " + response[i].name + "</li><li> Description " + response[i].description + "</li></div>"  );
+      $(".printName").append("<div class = 'project'><a href = '"+response[i].html_url+"'><li class = 'red'> Name: " + response[i].name + "</li></a>");
+      $(".printDescription").append("<div class = 'project'><li> Description " + response[i].description + "</li></div>"  );
+      $(".printDate").append("<div class = 'project'><li> Date " + response[i].created_at + "</li></div>"  );
 
     }
 
