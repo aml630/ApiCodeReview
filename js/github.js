ac306f9a89c889b46120eeb38b00c4ac4275ad6e
alex <alex.m.larson@gmail.com>
2016-03-18 09:01:44 -0700

    add name and description search functionality

--- js/github.js
+++ js/github.js
@@ -5,9 +5,11 @@ exports.gitHubSearch = function (userName) {
   $.get('https://api.github.com/users/'+userName+'/repos?access_token=' + apiKey).then(function(response){
     console.log(response.length);
     for(var i = 0; i < response.length; i  ++) {
-      $(".printOut").append("<div class = 'project'><li class = 'red'> Name: " + response[i].name + "</li>");
-      $(".printOut").append("<li> Description " + response[i].description + "</li>");
-      $(".printOut").append("</div>")
+      $(".printOut").append("<div class = 'project'><li class = 'red'> Name: " + response[i].name + "</li>" + );
+
+      $(".printOut").append("<li class = 'last'> Description " + response[i].description + "</li>");
+
+      $(".printOut .last").append("</div>");
     }
 
   }).fail(function(error){
