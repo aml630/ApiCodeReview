a539003c686aae550dccb796abcdc78679417dae
alex <alex.m.larson@gmail.com>
2016-03-18 09:10:48 -0700

    add picture search functionality

--- js/github.js
+++ js/github.js
@@ -3,13 +3,11 @@ var apiKey  = require('./../.env').apiKey;
 exports.gitHubSearch = function (userName) {
 
   $.get('https://api.github.com/users/'+userName+'/repos?access_token=' + apiKey).then(function(response){
-    console.log(response.length);
+    console.log(response);
+    $(".printOut").append("<img class = 'round' src = '"+ response[0].owner.avatar_url + "'>")
     for(var i = 0; i < response.length; i  ++) {
-      $(".printOut").append("<div class = 'project'><li class = 'red'> Name: " + response[i].name + "</li>" + );
+      $(".printOut").append("<div class = 'project'><li class = 'red'> Name: " + response[i].name + "</li><li> Description " + response[i].description + "</li></div>"  );
 
-      $(".printOut").append("<li class = 'last'> Description " + response[i].description + "</li>");
-
-      $(".printOut .last").append("</div>");
     }
 
   }).fail(function(error){
